f30de3a64131df0e156ed2e2e355c4ce1af11dec
Aurimar Bezerra Melo de Sousa Filho <aurimar.filho@qitech.work>
2021-02-24 22:06:53 -0300

    added complete challange, coutdown, expirence bar and profile components and made base project for move it application

---
+++ src/components/Profile.tsx
@@ -0,0 +1,18 @@
+import styles from '../styles/components/Profile.module.css'
+
+export function Profile() {
+    return (
+        <div className={styles.profileContainer}>
+            <img src="https://instagram.fgru5-1.fna.fbcdn.net/v/t51.2885-19/154046481_800644220661130_2653907380726759356_n.jpg?_nc_ht=instagram.fgru5-1.fna.fbcdn.net&_nc_ohc=amjYFFlFGAUAX9hQVUo&oh=63d3bd39c6eb7ab1217ab7472c221c44&oe=6061574E" alt="Aurimar Bezerra" />
+            <div>
+                <strong>
+                    Aurimar Bezerra
+                </strong>
+                <p>
+                    <img src="icons/level.svg" alt="Level" />
+                    Level 1
+                </p>
+            </div>
+        </div>
+    );
+}
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,6 @@
-import '../styles/globals.css'
+// Page thats is used for components that are always showed in the page
+
+import '../styles/global.css'
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
