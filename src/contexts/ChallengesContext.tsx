169a10dc8a9830580b67dd7a5f73a34b5ccdbd7f
Aurimar Bezerra Melo de Sousa Filho <aurimar.filho@qitech.work>
2021-02-27 10:21:54 -0300

    added cookies to save level, experience and completed data info

--- src/contexts/ChallengesContext.tsx
+++ src/contexts/ChallengesContext.tsx
@@ -1,4 +1,6 @@
-import { createContext, useState, ReactNode, useEffect } from 'react';
+import { createContext, useState, ReactNode, useEffect } from 'react'
+// packages made with javascrip do not has typing add type with @types in development;
+import Cookies from 'js-cookie';
 import challenges from '../../challenges.json';
 
 interface challenge {
@@ -20,16 +22,19 @@ interface ChallengesContextData {
 }
 
 interface ChallengesProviderProps {
-    children: ReactNode
+    children: ReactNode;
+    level: number;
+    currentExperience: number;
+    challengesCompleted: number;
 }
 
 
 export const ChallengesContext = createContext({} as ChallengesContextData);
 
-export function ChallengesProvider({ children }: ChallengesProviderProps) {
-    const [level, setLevel] = useState(1);
-    const [currentExperience, setCurrentExperience] = useState(0);
-    const [challengesCompleted, setChallengesCompleted] = useState(0);
+export function ChallengesProvider({ children, ...rest }: ChallengesProviderProps) {
+    const [level, setLevel] = useState(rest.level ?? 1);
+    const [currentExperience, setCurrentExperience] = useState(rest.currentExperience ?? 0);
+    const [challengesCompleted, setChallengesCompleted] = useState(rest.challengesCompleted ?? 0);
     const [activeChallenge, setActiveChallenge] = useState(null);
 
     const experienceToNextLevel = Math.pow((level + 1) * 4, 2);
@@ -38,6 +43,12 @@ export function ChallengesProvider({ children }: ChallengesProviderProps) {
     useEffect(() => {
         Notification.requestPermission();
     }, []);
+
+    useEffect(() => {
+        Cookies.set('level', String(level));
+        Cookies.set('currentExperience', String(currentExperience));
+        Cookies.set('challengesCompleted', String(challengesCompleted));
+    }, [level, currentExperience, challengesCompleted]);
 
     function levelUp() {
         setLevel(level + 1);
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,33 +4,62 @@ import { Profile } from "../components/Profile";
 import { CompletedChanlleges } from '../components/CompletedChallenges';
 import { Countdown } from '../components/Coutdown';
 
-import Head from 'next/head'
+import Head from 'next/head';
+import { GetServerSideProps } from 'next';
 import { ChanllegeBox } from '../components/ChallengeBox';
 import { CountdownProvider } from '../contexts/CoutdownContext';
+import { ChallengesProvider } from '../contexts/ChallengesContext';
 
-export default function Home() {
+interface HomeProps {
+  level: number;
+  currentExperience: number;
+  challengesCompleted: number;
+}
+
+export default function Home(props: HomeProps) {
   return (
     <div className={styles.container}>
-      <Head>
-        <title>Início | move.it</title>
-      </Head>
+      <ChallengesProvider
+        level={props.level}
+        currentExperience={props.currentExperience}
+        challengesCompleted={props.challengesCompleted}
+      >
 
-      <ExperienceBar />
+        <Head>
+          <title>Início | move.it</title>
+        </Head>
 
-      <CountdownProvider>
-        <section>
-          <div>
-            <Profile />
-            <CompletedChanlleges />
-            <Countdown />
-          </div>
-          <div>
-            <ChanllegeBox />
-          </div>
-        </section>
-      </CountdownProvider>
+        <ExperienceBar />
+
+        <CountdownProvider>
+          <section>
+            <div>
+              <Profile />
+              <CompletedChanlleges />
+              <Countdown />
+            </div>
+            <div>
+              <ChanllegeBox />
+            </div>
+          </section>
+        </CountdownProvider>
+      </ChallengesProvider>
     </div>
-
-
   )
+}
+
+
+// needs to be async and has this name getServerSideProps
+// run before react rendering
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+
+  const { level, currentExperience, challengesCompleted } = ctx.req.cookies
+
+  return ({
+    props: {
+      level: Number(level),
+      currentExperience: Number(currentExperience),
+      challengesCompleted: Number(challengesCompleted)
+    }
+  });
 }
